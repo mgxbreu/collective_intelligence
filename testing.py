50a9bc5334b83188e89899179060954e611c34f6
Mariam Garcia <20163810@itla.edu.do>
2023-03-30 04:48:18 -0400

    🔇

--- testing.py
+++ testing.py
@@ -41,21 +41,16 @@ class GeneticAlgorithmTSP():
             if current_distance == np.inf:
                 return MAX_NUMBER
             total_distance += current_distance
-        #check why
-        # total_distance += distance[path[-1], path[0]]
         return total_distance
 
     def _generate_fitness_list(self):
         self.fitness_list = [self._fitness(chromosomes) for chromosomes in self.population]
 
-    #error on key
     def _parent_selection(self):
         self._generate_fitness_list()
         indices = np.argsort(self.fitness_list)
-        # check why
         parents_selected = [self.population[i] for i in indices[:self.selection_count]]
         return parents_selected
-#
     
     def _crossover(self, parent1, parent2):
         half_parent_genes = int(len(parent1) / 2)
@@ -70,11 +65,9 @@ class GeneticAlgorithmTSP():
 
         child_to_be_born1 = section1 + [gene if gene not in section1 else 'X' for gene in child1[half_parent_genes:]]
         child_to_be_born2 = section2 + [gene if gene not in section2 else 'X' for gene in child2[half_parent_genes:]]
-        # print(child_to_be_born1)
 
         for idx, gene in enumerate(child_to_be_born1):
             if gene == 'X':
-                # print(mapped_genes1[1])
                 gene_mapped = child1[idx]
 
                 while gene_mapped in child_to_be_born1:
@@ -84,7 +77,6 @@ class GeneticAlgorithmTSP():
 
         for idx, gene in enumerate(child_to_be_born2):
             if gene == 'X':
-                # print(mapped_genes1[1])
                 gene_mapped = child2[idx]
 
                 while gene_mapped in child_to_be_born2:
@@ -107,21 +99,6 @@ class GeneticAlgorithmTSP():
         print(self.population)
         parents = self._parent_selection()
         print(parents)
-        # parent1 = parents[0]
-        # parent2 = parents[1]
-        # children = self._crossover(parent1, parent2)
-        # print(children)
-        # for child in children:
-        #     if child in parents:
-        #         children.remove(child)
-        # print(self.fitness_list)
-        # self._generate_fitness_list()
-        # self._find_best_path()
-        
         
 TSP = GeneticAlgorithmTSP()
 TSP.solve()
-
-#TODO: mutacion
-#TODO: fix errors
-#TODO: make class receive params
