8a9db8fedc7bef75f382535b420b158284fbd96a
Mariam Garcia <20163810@itla.edu.do>
2023-05-02 19:02:02 -0400

    🚚 move graph to separate folder

--- testing.py
+++ testing.py
@@ -1,28 +1,27 @@
-from graph import Graph
+from classes.graph import Graph
 import numpy as np
 from constants import MAX_NUMBER
 import math
 import random
 from algorithms.genetic_algorithm import GeneticAlgorithmTSP
- 
+
 nodes = 6
 cities = Graph(nodes)
-cities.add_edge(0,1,3)
-cities.add_edge(0,2,4)
-cities.add_edge(0,3,5)
-cities.add_edge(0,4,6)
-cities.add_edge(0,5,7)
-cities.add_edge(1,2,8)
-cities.add_edge(1,3,9)
-cities.add_edge(1,4,10)
-cities.add_edge(1,5,11)
-cities.add_edge(2,3,12)
-cities.add_edge(2,4,13)
-cities.add_edge(2,5,14)
-cities.add_edge(3,4,15)
-cities.add_edge(3,5,16)
-cities.add_edge(4,5,17)
-
+cities.add_edge(0, 1, 3)
+cities.add_edge(0, 2, 4)
+cities.add_edge(0, 3, 5)
+cities.add_edge(0, 4, 6)
+cities.add_edge(0, 5, 7)
+cities.add_edge(1, 2, 8)
+cities.add_edge(1, 3, 9)
+cities.add_edge(1, 4, 10)
+cities.add_edge(1, 5, 11)
+cities.add_edge(2, 3, 12)
+cities.add_edge(2, 4, 13)
+cities.add_edge(2, 5, 14)
+cities.add_edge(3, 4, 15)
+cities.add_edge(3, 5, 16)
+cities.add_edge(4, 5, 17)
 
 
 distance = cities.matrix
