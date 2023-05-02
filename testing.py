939d683d4ffe81615b720a0f6803da1beb6685f3
Mariam Garcia <20163810@itla.edu.do>
2023-05-02 18:35:13 -0400

    :white_check_mark: change test

--- testing.py
+++ testing.py
@@ -5,14 +5,25 @@ import math
 import random
 from genetic_algorithm import GeneticAlgorithmTSP
  
-nodes = 4
+nodes = 6
 cities = Graph(nodes)
 cities.add_edge(0,1,3)
+cities.add_edge(0,2,4)
 cities.add_edge(0,3,5)
-cities.add_edge(1,0,2)
-cities.add_edge(1,3,4)
-cities.add_edge(2,1,1)
-cities.add_edge(3,2,2)
+cities.add_edge(0,4,6)
+cities.add_edge(0,5,7)
+cities.add_edge(1,2,8)
+cities.add_edge(1,3,9)
+cities.add_edge(1,4,10)
+cities.add_edge(1,5,11)
+cities.add_edge(2,3,12)
+cities.add_edge(2,4,13)
+cities.add_edge(2,5,14)
+cities.add_edge(3,4,15)
+cities.add_edge(3,5,16)
+cities.add_edge(4,5,17)
+
+
 
 distance = cities.matrix
 
